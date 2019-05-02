d30dbb301476ce4b4de6f4cfdb1c06e42883e797
Richard Huynh <richard.c.huynh@gmail.com>
2019-05-02 15:45:23 -0700

    Update proxy for deployed EC2 instances

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
+require('newrelic');
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const path = require('path');
 const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 
-app.get('/', (req, res) => res.redirect(`/bookings/${Math.floor(Math.random()* 100)}`));
+app.get('/', (req, res) => res.redirect(`/homes/${Math.floor(Math.random()* 10000000)}`));
 
-app.use('/bookings/:roomid', express.static(path.join(__dirname, '../public')));
+app.use('/homes/:host_id', express.static(path.join(__dirname, '../public')));
 
 app.get('/photosandcomments/:accommodationid', (req, res) => {
   axios.get(`http://localhost:3001/photosandcomments/${req.params.accommodationid}`)
@@ -40,8 +41,9 @@ app.get('/abodes/:abode_id/reviews', (req, res) => {
     });
 });
 
-app.get('/homes', (req, res) => {
-  axios.get(`http://localhost:3004/homes`)
+app.get('/homes/:host_id/nearby', (req, res) => {
+  const hostID = req.params.host_id;
+  axios.get(`ec2-54-183-57-67.us-west-1.compute.amazonaws.com/homes/${hostID}/nearby`)
     .then(response => {
       res.send(response.data);
     });
